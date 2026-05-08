4cf292ce6df2feca30bcc4146d89b6aef8b2ecbb
omni-global <sovanroy88@gmail.com>
2026-05-08 11:43:05 +0530

    Delete page.tsx

--- page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Page() {
-  return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Omni App Live Ho Gayi Hai!</h1>
-      <p>Humara setup successful raha.</p>
-    </div>
-  )
-}
